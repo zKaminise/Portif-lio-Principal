9fccd48b2f2ec1c712cf340d21970a4c73bfd72b
gabriel.kaminise <gabriel.kaminise@sankhya.com.br>
2025-03-04 02:35:39 -0300

    Criação de Portifólio Profissional com destaque para projetos e contato para orçamento de sites

---
+++ JS/typer.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".typing", {
+  strings: ["Desenvolvedor Back End", "Desenvolvedor Full Stack", "Criador de Sites"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  startDelay: 500,
+  backDelay: 1500,
+  loop: true
+});
